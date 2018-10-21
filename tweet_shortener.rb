c47539bdf387f69094ddcfbc87ee92f9af3cb7c5
Cristian  CC <cristi.nord@gmail.com>
2018-10-21 19:39:33 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -34,7 +34,7 @@ def selective_tweet_shortener(tweets)
   tweets.split(" ").map do |tweet|
     if tweet.length > 140
       word_substituter(tweet)
-    else 
+    elsif tweet.length <= 140
       tweet
     end
   end
